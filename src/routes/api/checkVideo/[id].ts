abd1820d1a5cc6da297084fa64d7555021e5ae4c
Dean Fogarty <angrytongan@gmail.com>
2022-01-20 20:56:50 +1000

    Oops.

--- src/routes/api/checkVideo/[id].ts
+++ src/routes/api/checkVideo/[id].ts
@@ -1,9 +1,13 @@
-const shotStackEndpoint = `https://api.shotstack.io/${import.meta.env.VITE_SHOTSTACK_ENVIRONMENT}`;
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const shotStackEndpoint = `https://api.shotstack.io/${process.env.VITE_SHOTSTACK_ENVIRONMENT}`;
 
 export const get = async ({ params }) => {
     const headers = {
         accept: 'application/json',
-        'x-api-key': import.meta.env.VITE_SHOTSTACK_API_KEY,
+        'x-api-key': process.env.SHOTSTACK_API_KEY,
     };
 
     const res = await fetch(`${shotStackEndpoint}/render/${params.id}`, {
